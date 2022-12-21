c267355211e68645a4bafccfe6d38cc32eef1785
cortner <christohortner@gmail.com>
2022-12-21 08:00:59 +0100

    draft site e gradient

--- src/evaluator.jl
+++ src/evaluator.jl
@@ -1,14 +1,19 @@
 
 module Evaluator
 
-import ACE1
-using ACEcore, JuLIP, Polynomials4ML
+import ACE1, ObjectPools, ACEcore, Polynomials4ML
+
+using ACEcore, JuLIP, ForwardDiff
 
 using ACEcore: SparseSymmProdDAG, PooledSparseProduct
 using LinearAlgebra: dot, norm 
 import ACE1: PIPotential
 import Polynomials4ML: CYlmBasis, OrthPolyBasis1D3T
 
+# This envelope interface needs to be redesigned 
+# at the moment, because we have two inputs, r anx x, 
+# one needs to return 2 derivatives, the r and the x derivative. 
+# this makes it a bit awkward to use. 
 struct XEnvelope{T}
    xl::T 
    xr::T 
@@ -19,10 +24,14 @@ end
 evaluate(env::XEnvelope, r, x) = 
       (x - env.xl)^(env.pl) * (env.xr - x)^(env.pr)
 
+evaluate_d(env::XEnvelope, r, x) = 
+      ForwardDiff.derivative(x -> evaluate(env, r, x), x)
+
 struct TransformedRadial{TB, TT, TE}
    basis::TB 
    trans::TT 
    envelope::TE 
+   # ------- temps 
 end
 
 function TransformedRadial(Pr::ACE1.OrthPolys.TransformedPolys)
@@ -59,11 +68,33 @@ function evaluate!(RR, basis::TransformedRadial, rr::AbstractVector{<: Real})
    return nothing 
 end
 
+import Polynomials4ML: evaluate_ed
+
+function evaluate_ed(basis::TransformedRadial, rr::AbstractVector{<: Real})
+   RR = zeros(length(rr), length(basis))
+   dRR = zeros(length(rr), length(basis))
+   evaluate_ed!(RR, dRR, basis, rr)
+   return RR, dRR 
+end
+
+function evaluate_ed!(RR, dRR, basis::TransformedRadial, rr::AbstractVector{<: Real})
+   xx = ACE1.Transforms.transform.(Ref(basis.trans), rr)
+   xx_d = ACE1.Transforms.transform_d.(Ref(basis.trans), rr)
+   ee = evaluate.(Ref(basis.envelope), rr, xx)
+   ee_d = evaluate_d.(Ref(basis.envelope), rr, xx)
+   Polynomials4ML.evaluate_ed!(RR, dRR, basis.basis, xx)
+   dRR .= (RR .* ee_d .+ dRR .* ee) .* xx_d
+   RR .= RR .* ee
+   return nothing 
+end
+
+
+
 struct NewEvaluator{T, TBR}
    bR::TBR
    bY::CYlmBasis{T}
    bA::PooledSparseProduct{2}
-   bAA::SparseSymmProdDAG{T}
+   bAA::SparseSymmProdDAG{Complex{T}}
    params::Vector{T}
 end
 
@@ -83,7 +114,7 @@ function NewEvaluator(V::PIPotential)
    iAA2iA = V.pibasis.inner[1].iAA2iA 
    orders = V.pibasis.inner[1].orders
    new_spec = [ iAA2iA[i, 1:orders[i]][:] for i = 1:length(orders) ]
-   bAA = ACEcore.SparseSymmProdDAG(new_spec)
+   bAA = ACEcore.SparseSymmProdDAG(new_spec, T = ComplexF64)
 
    return NewEvaluator(bR, bY, bA, bAA, copy(V.coeffs[1]))
 end
@@ -91,12 +122,22 @@ end
 ACE1.evaluate(V::NewEvaluator, Rs, Zs, z0) = 
       ACE1.evaluate!(nothing, V::NewEvaluator, Rs, Zs, z0)
 
+
+function real_dot(a::AbstractVector{T}, b::AbstractVector) where {T <: Real}
+   val = zero(T) 
+   @assert length(a) == length(b) 
+   @simd ivdep for i = 1:length(a)
+      @inbounds val += a[i] * real(b[i])
+   end
+   return val 
+end
+
 function ACE1.evaluate!(tmp, V::NewEvaluator, Rs, Zs, z0)
    Rn = evaluate(V.bR, norm.(Rs))
    Ylm = Polynomials4ML.evaluate(V.bY, Rs)
    A = ACEcore.evalpool(V.bA, (Rn, Ylm))
    AA = ACEcore.evaluate(V.bAA, A)
-   val = dot(V.params, real.(AA))
+   val = real_dot(V.params, AA)
    
    Polynomials4ML.release!(Ylm)
    ACEcore.release!(A)
@@ -104,5 +145,59 @@ function ACE1.evaluate!(tmp, V::NewEvaluator, Rs, Zs, z0)
 
    return val 
 end
+
+
+
+
+function ACE1.evaluate_d!(dEs, tmp, V::NewEvaluator, Rs, Zs, z0)
+   # the embeddings are differentiated in forward-mode 
+   Rn, Rn_d = evaluate_ed(V.bR, norm.(Rs))
+   Ylm, Ylm_d = Polynomials4ML.evaluate_ed(V.bY, Rs)
+
+   # the rest in backward mode
+   # first the forward pass 
+   A = ACEcore.evalpool(V.bA, (Rn, Ylm))
+   AA = ACEcore.evaluate(V.bAA, A)
+   val = real_dot(V.params, AA)
+
+   # and the backward pass 
+   # [∂AA] := ∂V / ∂AA = params 
+   ∂AA = V.params    
+   # ∂V / ∂A = [∂AA] * ∂AA / ∂A
+   T∂A = promote_type(eltype(∂AA), eltype(A))
+   ∂A = zeros(T∂A, length(A))
+   ACEcore.pullback_arg!(∂A, ∂AA, V.bAA, AA)
+   # ∂V / ∂P = [∂A] * ∂A / ∂P
+   ∂Rn, ∂Ylm = ACEcore._pullback_evalpool(∂A, V.bA, (Rn, Ylm))
+
+   # now we have the pullback all the way to the embeddings and 
+   # can now combine this with the forward-mode derivatives 
+   @assert length(dEs) >= length(Rs)
+   # TODO: need more asserts once we move this to @inbounds 
+
+   fill!(dEs, zero(eltype(dEs)))
+   for n = 1:size(Rn, 2)
+      for i = 1:length(Rs)
+         rr = Rs[i]
+         r = norm(rr)
+         dEs[i] += (Rn_d[i, n] * real(∂Rn[n]) / r) * rr
+      end
+   end
+   for lm = 1:size(Ylm, 2)
+      for i = 1:length(Rs)
+         dEs[i] += real.(Ylm_d[i, lm] * ∂Ylm[lm])
+      end
+   end
+
+   Polynomials4ML.release!(Ylm)
+   ACEcore.release!(A)
+   ACEcore.release!(AA)
+   ACEcore.release!(∂Rn)
+   ACEcore.release!(∂Ylm)
+
+   return dEs
+end
+
+
 
 end
--- test/test_evaluator.jl
+++ test/test_evaluator.jl
@@ -2,7 +2,7 @@
 
 ##
 
-using ACE1, ACE1.Testing, ACEcore
+using ACE1, ACE1.Testing, ACEcore, BenchmarkTools
 using Printf, Test, LinearAlgebra, JuLIP, JuLIP.Testing, Random
 using JuLIP: evaluate, evaluate_d, evaluate_ed
 using ACE1: combine
@@ -29,56 +29,7 @@ Rs, Zs, z0 = ACE1.rand_nhd(Nat, Pr, :X)
 
 ##
 
-@info("check the translation of the radial basis")
-rr = norm.(Rs)
-xx = Pr.trans.(rr)
-P_old  = collect(hcat(evaluate.(Ref(Pr.J), xx)...)')
-Pr_new = ACE1x.Evaluator.TransformedRadial(Pr)
-P_new = ACE1x.Evaluator.evaluate(Pr_new, rr)
-@show P_old ≈ P_new
-
-##
-
-@info("A basis")
-bY = Polynomials4ML.CYlmBasis(V.pibasis.basis1p.SH.maxL)
-bR = ACE1x.Evaluator.TransformedRadial(V.pibasis.basis1p.J)
-
-A_spec = V.pibasis.basis1p.spec
-A_spec_new = Tuple{Int,Int}[] 
-for b in A_spec 
-   n = b.n 
-   iY = Polynomials4ML.index_y(b.l, b.m)
-   push!(A_spec_new, (n, iY))
-end
-bA = ACEcore.PooledSparseProduct(A_spec_new)
-
-# new method 
-Rn = ACE1x.Evaluator.evaluate(bR, rr)
-Ylm = Polynomials4ML.evaluate(bY, Rs)
-A_new = ACEcore.evalpool(bA, (Rn, Ylm))
-
-# old method 
-A_old = ACE1.evaluate(V.pibasis.basis1p, Rs, Zs, z0)
-
-@show A_new ≈ A_old
-
-##
-
-@info("AA basis")
-
-iAA2iA = V.pibasis.inner[1].iAA2iA 
-orders = V.pibasis.inner[1].orders
-new_spec = [ iAA2iA[i, 1:orders[i]][:] for i = 1:length(orders) ]
-bAA = ACEcore.SparseSymmProdDAG(new_spec)
-
-AA_new = ACEcore.evaluate(bAA, A_new)
-AA_old = ACE1.evaluate(V.pibasis, Rs, Zs, z0)
-
-@show AA_new ≈ AA_old
-
-##
-
-@info("full model")
+@info("compare full model")
 V_new = ACE1x.Evaluator.NewEvaluator(V)
 
 val_new = ACE1.evaluate(V_new, Rs, Zs, z0)
@@ -87,14 +38,70 @@ val_old = ACE1.evaluate(V, Rs, Zs, z0)
 @show val_new ≈ val_old
 
 ##
-# print("     energy: ")
-# println(@test energy(V, at) ≈ naive_energy(V, at) )
-# print("site-energy: ")
-# println(@test energy(V, at) ≈ sum( site_energy(V, at, n)
-#                                       for n = 1:length(at) ) )
-# print("     forces: ")
-# println(@test JuLIP.Testing.fdtest(V, at; verbose=false))
-# print("site-forces: ")
-# println(@test JuLIP.Testing.fdtest( x -> site_energy(V, set_dofs!(at, x), 3),
-#                                     x -> mat(site_energy_d(V, set_dofs!(at, x), 3))[:],
-#                                     dofs(at); verbose=false ) )
+
+Nat = 50
+Rs, Zs, z0 = ACE1.rand_nhd(Nat, Pr, :X)
+
+@btime ACE1.evaluate($V, $Rs, $Zs, $z0)
+
+@btime ACE1.evaluate($V_new, $Rs, $Zs, $z0)
+
+##
+
+# @profview let V = V_new, Rs = Rs, Zs = Zs, z0 = z0
+#    for _ = 1:20_000 
+#       ACE1.evaluate(V, Rs, Zs, z0)
+#    end
+# end
+
+##
+
+bA = V_new.bA
+cY = randn(ComplexF64, 50, length(V_new.bY))
+rY = randn(50, length(V_new.bY))
+Rn = randn(50, length(V_new.bR))
+
+cA = zeros(ComplexF64, length(bA))
+rA = zeros(length(bA))
+
+@info("complex evalpool")
+@btime ACEcore.evalpool!($cA, $bA, ($Rn, $cY))
+@info("real evalpool")
+@btime ACEcore.evalpool!($rA, $bA, ($Rn, $rY))
+
+##
+
+rbY = Polynomials4ML.RYlmBasis(5)
+cbY = Polynomials4ML.CYlmBasis(5)
+@info("Complex Ylm")
+@btime (Y = Polynomials4ML.evaluate($cbY, $Rs); 
+        Polynomials4ML.release!(Y))
+@info("Real Ylm")
+@btime (Y = Polynomials4ML.evaluate($rbY, $Rs); 
+        Polynomials4ML.release!(Y))
+
+##
+
+dEs2 = zeros(JVecF, length(Rs))
+dEs2 = ACE1.evaluate_d!(dEs2, nothing, V_new, Rs, Zs, z0)
+
+dEs1 = ACE1.evaluate_d(V, Rs, Zs, z0)
+
+dEs1 ≈ dEs2 
+
+
+
+
+
+##
+
+
+# using Printf 
+
+# rr =  norm.(Rs)
+# Rn, Rn_d = ACE1x.Evaluator.evaluate_ed(V_new.bR, rr)
+# for h in (0.1).^(2:10)
+#    Rn_h = ACE1x.Evaluator.evaluate(V_new.bR, rr .+ h)
+#    Rn_dh = (Rn_h - Rn) / h 
+#    @printf(" %.2e  |  %.2e \n", h, norm(Rn_d - Rn_dh, Inf))   
+# end
