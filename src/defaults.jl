8203a6ff5bb3569505914941476177f1c7a9225b
cortner <christohortner@gmail.com>
2023-02-07 22:39:34 -0800

    towards new best heuristics

--- src/defaults.jl
+++ src/defaults.jl
@@ -1,25 +1,212 @@
 
-import YAML
-using JuLIP: AtomicNumber
+import YAML, ACE1 
+using NamedTupleTools: namedtuple
+using JuLIP: AtomicNumber, rnn 
+using ACE1.Transforms: agnesi_transform, multitransform
+using ACE1.OrthPolys: transformed_jacobi, transformed_jacobi_env
 
 # -------------- Bond-length heuristics 
 
 _lengthscales_path = joinpath(@__DIR__, "..", "data", 
                               "length_scales_VASP_auto_length_scales.yaml")
-_lengthscales = YAML.load_file(lengthscales_path)
+_lengthscales = YAML.load_file(_lengthscales_path)
 
+get_bond_len(s::Symbol) = get_bond_len(AtomicNumber(s))
 get_bond_len(z::AtomicNumber) = get_bond_len(convert(Int, z)) 
 
 function get_bond_len(z::Integer) 
    if haskey(_lengthscales, z)
-      return _lengthscales[z]["bond_len"]
-   end
+      return _lengthscales[z]["bond_len"][1]
+   elseif rnn(AtomicNumber(z)) > 0 
+      return rnn(AtomicNumber(z)) 
+   end 
    error("No typical bond length for atomic number $z is known. Please specify manually.")
 end
 
-get_r0(z1, z2) = get_bond_len(z1) + get_bond_len(z2)
+get_r0(z1, z2) = (get_bond_len(z1) + get_bond_len(z2)) / 2
 
 
 
 # -------------- ACE basis with good defaults 
 
+const _kw_defaults = Dict(:elements => nothing, 
+                          :order => nothing, 
+                          :totaldegree => nothing, 
+                          :wL => 1.5, 
+                          :r0 => :bondlen, 
+                          :rcut => (:bondlen, 2.5),
+                          :transform => (:agnesi, 2, 4),
+                          :envelope => (:x, 2, 2),
+                          :rbasis => :legendre, 
+                          :pure2b => true, 
+                          :delete2b => true, 
+                          #
+                          :pair_rcut => :pair, 
+                          :pair_degree => :totaldegree,
+                          :pair_transform => (:agnesi, 1, 3), 
+                          :pair_basis => :legendre, 
+                          # 
+                          )
+
+const _kw_aliases = Dict( :N => :order, 
+                          :species => :elements, 
+                          :trans => :transform, 
+                         )
+
+function _clean_args(kwargs) 
+   dargs = Dict{Symbol, Any}() 
+   for key in keys(kwargs)
+      if haskey(ACE1x._kw_aliases, key) 
+         dargs[ACE1x._kw_aliases[key]] = kwargs[key]
+      else 
+         dargs[key] = kwargs[key]
+      end
+   end 
+   for key in keys(_kw_defaults) 
+      if !haskey(dargs, key) 
+         dargs[key] = _kw_defaults[key]
+      end
+   end
+   return namedtuple(dargs)
+end
+
+function _get_order(kwargs) 
+   if haskey(kwargs, :order) 
+      return kwargs[:order] 
+   elseif haskey(kwargs, :bodyorder) 
+      return kwargs[:bodyorder] - 1 
+   end 
+   error("Cannot determine correlation order or body order of ACE basis from the arguments provided.")
+end
+
+function _get_degrees(kwargs) 
+
+   if haskey(kwargs, :totaldegree)
+      if kwargs[:totaldegree] isa Union{Number, Dict} 
+         cor_order = _get_order(kwargs)
+         Deg, maxdeg = ACE1.Utils._auto_degrees(cor_order, kwargs[:totaldegree], 
+                                                kwargs[:wL], nothing)
+         maxn = maxdeg 
+         return Deg, maxdeg, maxn 
+      end
+   end 
+   error("Cannot determine total degree of ACE basis from the arguments provided.")
+end
+
+function _get_r0(kwargs, z1, z2)
+   if kwargs[:r0] == :bondlen
+      return get_r0(z1, z2)
+   elseif kwargs[:r0] isa Number
+      return kwargs[:r0]
+   elseif kwargs[:r0] isa Dict
+      return kwargs[:r0][(z1, z2)]
+   end
+   error("Unable to determine r0($z1, $z2) from the arguments provided.")
+end
+
+function _get_all_r0(kwargs)
+   elements = kwargs[:elements]
+   r0 = Dict( [ (s1, s2) => _get_r0(kwargs, s1, s2) 
+                   for s1 in elements, s2 in elements]... )
+end
+
+function _get_rcut(kwargs, s1, s2) 
+   _rcut = kwargs[:rcut]
+   if _rcut isa Tuple 
+      if _rcut[1] == :bondlen
+         return _rcut[2] * get_r0(s1, s2)
+      end
+   elseif _rcut isa Number 
+      return _rcut 
+   elseif _rcut isa Dict 
+      return _rcut[(s1, s2)]
+   end
+   error("Unable to determine rcut($s1, $s2) from the arguments provided.")
+end
+
+function _get_all_rcut(kwargs) 
+   if kwargs[:rcut] isa Number 
+      return kwargs[:rcut]
+   end
+   elements = kwargs[:elements]
+   rcut = Dict( [ (s1, s2) => _get_rcut(kwargs, s1, s2) 
+                   for s1 in elements, s2 in elements]... )
+   return rcut 
+end
+
+function _transform(kwargs)
+   elements = kwargs[:elements]
+   transform = kwargs[:transform]
+
+   if transform isa Tuple
+      if transform[1] == :agnesi 
+         p = transform[2]
+         q = transform[3]
+         r0 = _get_all_r0(kwargs)
+         rcut = _get_all_rcut(kwargs)
+         transforms = Dict([ (s1, s2) => agnesi_transform(r0[(s1, s2)], p, q)
+                             for s1 in elements, s2 in elements]... )
+         trans_ace = multitransform(transforms; rin = 0.0, rcut = 8.0)
+         return trans_ace 
+      end
+   
+   elseif transform isa ACE1.Transforms.DistanceTransform 
+      return transform 
+   end
+
+   error("Unable to determine transform from the arguments provided.")
+end
+
+
+function _radial_basis(kwargs) 
+   rbasis = kwargs[:rbasis]
+
+   if rbasis == :legendre 
+      Deg, maxn, maxl = _get_degrees(kwargs)      
+      if kwargs[:pure2b] 
+         cor_order = _get_order(kwargs)
+         envelope = kwargs[:envelope] 
+         if envelope isa Tuple 
+            if envelope[1] == :x 
+               pin = envelope[2]
+               pcut = envelope[3]
+               maxn += (pin + pcut) * (cor_order-1)
+            end
+         else 
+            error("I can't construct the radial basis automatically without knowing the envelope.")
+         end
+      end 
+      trans_ace = _transform(kwargs)
+      Rn_basis = transformed_jacobi(maxn, trans_ace; pcut = pcut, pin = pin)
+      return Rn_basis
+
+   elseif rbasis isa ACE1.ScalarBasis
+      return rbasis 
+   end
+
+   error("Unable to determine the radial basis from the arguments provided.")
+end
+
+
+
+
+function ace_basis(; kwargs...)
+   kwargs = _clean_args(kwargs)
+   elements = kwargs[:elements]
+   cor_order = _get_order(kwargs)
+   Deg, maxn, maxdeg = _get_degrees(kwargs)
+   rbasis = _radial_basis(kwargs)
+
+   if kwargs[:pure2b] 
+      rpibasis = Pure2b.pure2b_basis(species = AtomicNumber.(elements),
+                              Rn=rbasis, 
+                              D=Deg,
+                              maxdeg=maxdeg, 
+                              order=cor_order, 
+                              delete2b = kwargs[:delete2b])
+   else
+      error("todo - implement standard ACE basis")
+   end 
+
+   return rpibasis
+end
---
+++ test/test_default.jl
@@ -0,0 +1,98 @@
+
+using ACE1
+using ACE1.Testing: print_tf, println_slim
+using ACE1.Transforms: AffineT, agnesi_transform, multitransform
+using ACE1: transformed_jacobi, transformed_jacobi_env
+using ACE1x
+using JuLIP
+using LinearAlgebra: Diagonal
+using Statistics: mean
+using Test 
+
+## ------- basic parameters 
+
+elements = [:Cs, :Pb, :Br] 
+
+# the following is a dumb way to pick the r0 values. The right way is to 
+# look at the bond-lengths and at the RDFs and come up with a sensible heuristic 
+# from those. 
+
+# atomic radii according to Google
+# r0_el = Dict(:Cs => 3.43, :Pb => 1.8, :Br => 1.85)
+# I'm correcting Cs since rnn(Cs) ≈ 1.5 * r0(Cs), but this is pure handwaving ... 
+r0_el = Dict( [s => ACE1x.get_bond_len(s)/2 for s in elements ]... )
+r0 = Dict( [(s1, s2) => (r0_el[s1] + r0_el[s2]) 
+            for s1 in elements, s2 in elements]... )
+
+pin = 2
+pcut = 2
+cor_order = 3
+maxdeg = 9
+maxdeg_pair = maxdeg 
+r_cut_ACE = 8.0
+r_cut_pair = r_cut_ACE
+
+## ------ Many-Body Basis 
+# transform for many-body basis. Note that rin = 0.0. If this 
+# doesn't work, then I need to make some changes to ACE1.jl
+# we should try with other parameters, but for now most combinations won't 
+# work due to a gap in the implementation of the Agnesi(p, q) transform.
+agnesi_p, agnesi_q = 2, 4
+transforms = Dict([ (s1, s2) => agnesi_transform(r0[s1, s2], agnesi_p, agnesi_q) 
+                    for s1 in elements, s2 in elements]... )
+trans_ace = multitransform(transforms; rin = 0.0, rcut = r_cut_ACE)
+ninc = (pcut + pin) * (cor_order-1)
+maxn = maxdeg + ninc 
+Pr_ace = transformed_jacobi(maxn, trans_ace; pcut = pin, pin = pin)
+
+rpibasis = ACE1x.Pure2b.pure2b_basis(species = AtomicNumber.(elements),
+                           Rn=Pr_ace, 
+                           D=ACE1.RPI.SparsePSHDegree(),
+                           maxdeg=maxdeg, 
+                           order=cor_order, 
+                           delete2b = true)
+
+## -------- Pair Basis 
+
+# the transform for the radial basis should be ok with (1, 4)
+ag_p_pair = 1; ag_q_pair = 3
+function _r_basis(s1, s2) 
+   _env = ACE1.PolyEnvelope(2, r0[(s1, s2)], r_cut_pair)
+   _trans = agnesi_transform(r0[s1, s2], ag_p_pair, ag_q_pair) 
+   _Pr = transformed_jacobi_env(maxdeg_pair, _trans, _env, r_cut_pair)
+   return _Pr 
+end
+
+Pr_pair = [ _r_basis(s1, s2) for s1 in elements, s2 in elements ]
+
+pair = PolyPairBasis(Pr_pair, elements)
+
+B = JuLIP.MLIPs.IPSuperBasis([pair, rpibasis]);
+basis_length = length(B)
+
+## ------- Automated Version, preliminary tests 
+@info("preliminary consistency tests")
+kwargs = (elements = elements, 
+          order = cor_order, 
+          totaldegree = maxdeg, 
+          rcut = r_cut_ACE )
+
+kwargs = ACE1x._clean_args(kwargs)
+
+println_slim(@test ACE1x._get_order(kwargs) == 3)
+
+# read all r0s. 
+_r0 = ACE1x._get_all_r0(kwargs)
+println_slim(@test _r0 == r0)
+
+ACE1x._get_all_rcut(kwargs)
+
+_trans = ACE1x._transform(kwargs)
+println_slim(@test _trans == trans_ace)
+
+_rbasis = ACE1x._radial_basis(kwargs)
+println_slim(@test _rbasis == Pr_ace)
+
+_rpibasis = ACE1x.ace_basis(; kwargs...)
+println_slim(@test _rpibasis == rpibasis)
+
